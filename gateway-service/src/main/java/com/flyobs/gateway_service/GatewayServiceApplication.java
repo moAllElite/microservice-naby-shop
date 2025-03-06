3ab1bfb9497e37737f806f4b094db5cb3a6e38c3
moAllElite <mouhamedn347@gmail.com>
2025-03-06 14:13:14 +0000

    Migrate spring boot verison from 3.4.0 to 3.3.9 on gateway service

--- gateway-service/src/main/java/com/flyobs/gateway_service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/flyobs/gateway_service/GatewayServiceApplication.java
@@ -2,6 +2,8 @@ package com.flyobs.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
 public class GatewayServiceApplication {
